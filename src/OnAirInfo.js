590d9eff28e8d1d19343ecaf418ef1b7ab3f8c2e
WojciechMadeja <wojciech-madeja@wp.pl>
2022-02-05 14:14:47 +0100

    Fix info about songs, artists etc.

--- src/OnAirInfo.js
+++ src/OnAirInfo.js
@@ -26,7 +26,7 @@ function OnAirInfo() {
           process.env.PUBLIC_URL+"/images/unknown-cover.png" : 
           (image.substring(0,27) === 'https://api.play.cz/static/') 
           ? process.env.PUBLIC_URL+"/images/unknown-cover.png" : image } alt='Artist' />
-        <p className="OnAirInfo-artist">{!artist ? 'Unknown Artist' : (artist === 'Pravé Rockové Rádio') ? 'Unknown Artist' : artist}</p>
+        <p className="OnAirInfo-artist">{artist ? (artist === 'Pravé Rockové Rádio') ? 'Unknown Artist' : artist : 'Unknown Artist'}</p>
         <p>{!song ? 'Unknown Song' : song}</p>
       </div>
   );
